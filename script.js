141aa96dc5950af88ca6d814ad05723674e02bed
oubijjane <79411385+oubijjane@users.noreply.github.com>
2024-09-01 11:46:23 +0100

    add the ability to play multipale rounds and more
    add score of the user and computer
    add user inpute to round number
    add support for case sensitive
    add support for wrong input

--- script.js
+++ script.js
@@ -9,20 +9,88 @@ creation a function result() to select the proper comparaison method
 depending in the value of the variable assinged to the function
 add the result() function to the userInput() function
 */
-let computerChoice = ["rock","paper","scissors"];
-let choice = prompt("Rock, Paper, or Scissors? ");
+let computerChoices = ["rock","paper","scissors"];
+let humanSelsection;
+let computerSelection 
 let humanScore = 0;
 let computerScore = 0;
 
 function getComputerChoice(number) {
-  return computerChoice[getRandomNUmber()];
+  return computerChoices[getRandomNUmber()];
 }
 
 function getRandomNUmber() {
     const minCeiled = Math.ceil(0);
-  const maxFloored = Math.floor(computerChoice.length);
+  const maxFloored = Math.floor(computerChoices.length);
   return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
 }
+
+function getHumanChoice() {
+    let choice = prompt("Rock, Paper, or Scissors? ");
+    return choice;
+}
+function win() {
+    humanScore += 1;
+    return humanScore;
+}
+
+function lose() {
+    computerScore += 1;
+
+    return computerScore;
+}
+
+function rock(x) {
+    console.log(x)
+    if(x === "scissors") {
+        return win();
+    }
+    return lose();
+}
+
+function paper(x) {
+    console.log(x)
+    if(x === "rock") {
+        return win();
+    }
+    return lose();
+}
+
+function scissors(x) {
+    console.log(x)
+    if(x === "paper") {
+        return win();
+    }
+    return lose();
+}
+
+let numberOfROund = prompt("number of rounds?");
+for(i = 0 ; i < numberOfROund; i++){
+
+    humanSelsection = getHumanChoice().toLowerCase();
+    computerSelection = getComputerChoice();
+
+function playRound(HumanChoice, computerChoice) {
+    console.log(HumanChoice +" " + computerChoice);
+    if(HumanChoice == computerChoice) {
+        return "no winner";
+    }
+    else if(HumanChoice == "paper") {
+         return paper(computerChoice);
+    }
+    else if(HumanChoice == "rock") {
+        return rock(computerChoice);
+   }
+   else if(HumanChoice == "scissors") {
+    return scissors(computerChoice);
+}
+
+    return "no valid choise";
+}
+console.log(playRound(humanSelsection, computerSelection));
+console.log("human score " + humanScore + " computer score " + computerScore)
+}
+
 
 
 
